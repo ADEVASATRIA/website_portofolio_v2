c1f67377772801fde6818ed2f09919727261320a
ADEVA SATRIA ARIF WIBAWA <adeva.wibawa@binus.ac.id>
2023-03-27 23:59:22 +0700

    (half-notsuccess)Sampai pemuatan input login & password dengan Scipt reactive

--- resources/js/components/auth/login.vue
+++ resources/js/components/auth/login.vue
@@ -1,12 +1,28 @@
+<script>
+    import { reactive } from 'vue'
+
+    let form = reactive({
+        email: '',
+        password: '',
+    })
+
+    const login = async()=>{
+        await axios.post('/api/login',form)
+            .then(response=>{
+                console.log(response)
+            })
+    }
+</script>
+
 <template>
     <div class="login">
         <div class="formLogin">
-            <form>
-                <input type="email" placeholder="Enter your email address" />
+            <form @submit.prevent="login">
+                <input type="email" placeholder="Enter email address" v-model="form.emai" />
                 <br>
-                <input type="password" placeholder="Enter your password" />
+                <input type="password" placeholder="Enter Password" v-model="form.password"/>
                 <br>
-                <input type="submit" value="Login" />
+                <input type="submit" value="Login" class="submit"/>
             </form>
         </div>
     </div>
@@ -36,15 +52,40 @@
         overflow: hidden;
     }
     input{
-        background-color: rgba(228, 232, 234, 0.8);
+        background-color: rgba(228, 232, 243, 0.8);
         background-position: 0.5em 0.6em;
         border: none;
         color: rgba(80, 80, 80, 1);
-        padding: 0 0 0 4rem;
+        padding: 0 0 0 0.3rem;
         margin: 0 0 lem 0;
-        width: 20em;
-        height: 2.8em;
+        width: 10em;
+        height: 2.1em;
         outline: none;
         transition: background-color 0.4s;
+    }
+    input:hover{
+        background-color: rgba(255, 255, 255, 255);
+    }
+    input:focus{
+        background-color: rgba(255, 255, 255, 255);
+    }
+    .submit{
+        color: rgba(35, 35, 35, 0.8);
+        background: rgba(220, 220, 220, 1);
+        padding: 0;
+        margin: 1.5em 0 0 0em;
+        width: 10em;
+        text-transform: uppercase;
+        cursor: pointer;
+        transition: background-color 0.4s;
+    }
+
+    .submit:hover{
+        background: #43467f;
+        color: #ffffff;
+    }
+    .submit:focus{
+        background: #43467f;
+        color: #ffffff;
     }
 </style>
